236f826396e7937365d89a8d9ed09c2c8b856b62
makalo <sokayloc@skiff.com>
2023-08-31 14:20:31 -0400

    fix promise error in js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 // variable for single puppy and puppy list when needed
-const singlePlayerDiv = document.getElementById("singlePuppy")
+const singlePlayerDiv = document.getElementById("singlePuppy");
 const playerListDiv = document.getElementById("pupbox");
 
 //data const to hold data from api
@@ -12,25 +12,39 @@ const data =  {
 
 // function to get all puppy info from api
 const getPuppies = async () => {
-   const fetchPuppy = await fetch(
-        'https://fsa-puppy-bowl.herokuapp.com/api/2307-FTB-ET-WEB-FT/players'
-    );
+   const fetchPuppy = await fetch
+   ("https://fsa-puppy-bowl.herokuapp.com/api/2307-FTB-ET-WEB-FT/players");
     const puppyData = await fetchPuppy.json();
-    console.log(puppyData);
-    data.allPlayers = puppyData.results
-
+    data.allPlayers = puppyData.data.players
+    console.log(data.allPlayers)
 }
 
+const renderAllPuppies = () => {
+    const puppies = data.allPlayers.map((player) => {
+        return `
+        <div>
+            <a href="#${player.id}"> </a>
+        </div>
+        
+        `
 
 
+    
+    
+        })
+
+    
+    playerListDiv.innerHTML = puppies.join('')
+}
 
 
 
 async function render() {
     await getPuppies()
-    
+    renderAllPuppies()
+
 }
 //
 
-
+render()
 
